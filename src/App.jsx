615bf78e1c4f765999d52350fcbbd032fb44ec5a
Md salim islam <salimislam0036@gmail.com>
2025-05-07 12:51:58 +0600

    first react application todo app

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { Todo } from "./TodoComponents/Todo"
+
+export const App = () =>{
+  return(
+    <Todo/>
+  )
+}
---
+++ src/TodoComponents/Todo.jsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import "./Todo.css";
+import { MdCheckCircle, MdDelete } from "react-icons/md";
+
+
+export const Todo = () =>{
+   // todo useState
+   const [inputValue,setInputValue] = useState('');
+   const [task,setTask] = useState([]);
+   const [dateTime,setDateTime] = useState("")
+
+   // todo time and date 
+   useEffect(()=>{
+      const interval = setInterval(()=>{
+         const now = new Date();
+         const formattedDate = now.toLocaleDateString();
+         const formattedTime = now.toLocaleTimeString();
+         setDateTime(`${formattedDate} - ${formattedTime}`)
+      },1000)
+      return ()=> clearInterval(interval);
+   },[])
+
+   // todo input field 
+   const handleInputChange = (value) =>{
+      setInputValue(value);
+   }
+
+   // todo form submit 
+   const handleInputSubmit = (e) =>{
+      e.preventDefault();
+
+      if(!inputValue) return;
+
+      if(task.includes(inputValue)) {
+         setInputValue('');
+         return;
+      }
+
+      setTask((prevTask)=>[...prevTask,inputValue]);
+
+      setInputValue('');
+   }
+
+
+
+    return (
+        <section className="main_section">
+           <div className="todo_container">
+
+               <header>
+                  <h1>TODO APPLICATION</h1>
+                  <h2>{dateTime}</h2>
+               </header>
+
+              <div className="form_field">
+                 <form className="form" onClick={handleInputSubmit}>
+                    <div className="input_add_task">
+                        <input type="text" name="name" value={inputValue} onChange={(e)=>handleInputChange(e.target.value)} />
+                        <button className="Add_task">Add Task</button>
+                    </div>
+                 </form>
+              </div>
+              <div className="list_items">
+               <ul>
+                  {
+                     task.map((currTask,index)=>{
+                        return (
+                           <li key={index}>
+                               <span>{currTask}</span>
+                               <div className="btn">
+                                  <button className="check"><MdCheckCircle /></button>
+                                  <button className="delete"><MdDelete /></button>
+                               </div>
+                           </li>
+                        )
+                     })
+                  }
+               </ul>
+              </div>
+              <button className="clear_all">
+                 Clear All
+              </button>
+           </div>
+        </section>
+    )
+}
